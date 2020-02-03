e05babafad5370012a3dbb2cff26eb6a3ceb7093
AlejandroAl <manuel@insaite.io>
2020-02-03 02:35:12 -0600

    ejercicio2 add progress

--- src/main/scala/ejercicio2/ejercicio2App.scala
+++ src/main/scala/ejercicio2/ejercicio2App.scala
@@ -1,27 +1,13 @@
 package ejercicio2
 
-import org.apache.spark.sql.{SparkSession, SaveMode}
+import org.apache.spark.sql.{SparkSession, SaveMode, DataFrame}
 import org.apache.spark.sql.functions._
 
 object app {
 
-  def main(args : Array[String]) {
-
-    var csvPath = args(0)
-
-    val spark = SparkSession.builder
-      .master("local[*]")
-      .appName("Ejercicio_1")
-      .config("spark.driver.memory", "4g")
-      .config("spark.executor.memory", "1g")
-      .config("spark.executor.cores", "8")
-      .config("spark.executor.memoryOverhead","0.10")
-      .config("es.index.auto.create", "true")
-      .getOrCreate()
+  def generaDiccionario(df: DataFrame, spark: SparkSession, pathDiccionario: String): Unit = {
     import spark.implicits._
-
-    val df_ejercicio2 = spark.read.option("header","true").option("inferSchema","true").csv(csvPath)
-    val df_ejercicio_grouped = df_ejercicio2.groupBy($"country").agg(countDistinct("country")).where($"country".isNotNull )
+    val df_ejercicio_grouped = df.groupBy($"country").agg(countDistinct("country")).where($"country".isNotNull )
 
     def getID = (country: String) => {
       val r = scala.util.Random
@@ -55,7 +41,57 @@ object app {
 
     df_diccionario_country.groupBy($"country_id").agg(count("country_id").alias("count")).where($"count" > 1).show(750)
 
-    df_diccionario_country.coalesce(1).write.option("mode",SaveMode.Overwrite.toString).option("header","true").csv("./diccionario")
+    df_diccionario_country.coalesce(1).write.mode(SaveMode.Overwrite).option("header","true").csv(pathDiccionario)
+  }
+
+  def main(args : Array[String]) {
+
+    var csvPath = args(0)
+    var pathDiccionario = args(1)
+
+    val spark = SparkSession.builder
+      .master("local[*]")
+      .appName("Ejercicio_1")
+      .config("spark.driver.memory", "4g")
+      .config("spark.executor.memory", "1g")
+      .config("spark.executor.cores", "8")
+      .config("spark.executor.memoryOverhead","0.10")
+      .config("es.index.auto.create", "true")
+      .getOrCreate()
+    import spark.implicits._
+
+    def trimColumn = (columnatext: String) => {
+      try {
+        columnatext.trim
+      } catch { case x: Exception => columnatext}
+
+    }
+
+    val trimColumna = spark.udf.register("trimColumna",trimColumn)
+
+    val df_ejercicio2 = spark.read.option("header","true").option("inferSchema","true").csv(csvPath)
+
+    val df_trim_ejercicio2 = df_ejercicio2.withColumn("country", trimColumna($"country"))
+
+    println("*********************************Total de registros actuales: "+df_ejercicio2.count())
+
+    df_trim_ejercicio2.show()
+
+//    generaDiccionario(df_ejercicio2,spark)
+
+    val df_diccionario = spark.read.option("header","true").option("inferSchema","true").csv(pathDiccionario)
+
+    println("*********************************Total de registros diccionario: "+df_diccionario.count())
+
+    val df_joined = df_trim_ejercicio2.join(df_diccionario,df_trim_ejercicio2.col("country") === df_diccionario.col("country"),"left")
+
+    println("*********************************Total de registros joined: "+df_joined.count())
+
+    println("*********************************Total de registros joined: "+df_joined.count())
+
+    println("*********************************Total de registros country null: "+df_joined.where($"country_id".isNull).count())
+
+    df_joined.where($"country_id".isNull).show()
 
   }
 
